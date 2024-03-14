c570d3d66acf96287f76fd5a6fe4c947592de403
nishantdotcom <nishantsingh1308@gmail.com>
2024-03-14 11:29:28 +0530

    setitng up project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-200">Hii</div>
+    </>
+  );
+}
+
+export default App;
